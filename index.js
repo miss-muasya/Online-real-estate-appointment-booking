f0748c1dc56889f8350be26041d6926ab365f2d9
miss-muasya <sheemuasya@gmail.com>
2022-09-07 14:07:56 +0300

    commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// /console.log("hello kitty")
+let button=document.querySelectorAll("#number-ofrooms a")
+console.log(button)
+    //  for(element of button){
+    //     element.addEventlistener("click",e=>{
+    //         console.log(e.target)
+    //     })
+        
+
+    //  }
