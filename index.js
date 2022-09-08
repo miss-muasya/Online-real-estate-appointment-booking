d163a2035b188f1eb12e0563ee787954f959b785
miss-muasya <sheemuasya@gmail.com>
2022-09-08 17:53:34 +0300

    project

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 // /console.log("hello kitty")
-let button=document.querySelectorAll("#number-ofrooms a")
-console.log(button)
-    //  for(element of button){
-    //     element.addEventlistener("click",e=>{
-    //         console.log(e.target)
-    //     })
+// let button=document.querySelectorAll("#number-ofrooms a")
+//console.log(button)
+    // for(element of button){
+       // element.addEventlistener("click",e=>{
+          //  console.log(e.target)
+    //    })
         
 
-    //  }
+   //  }
+   
+   function bookAlert(){
+       alert("Booked successfully")
+   }
+
+    function searchhouse(searchTerm) {
+       getSearchMethod(searchTerm);
+
+      fetch("http://localhost:3000/posts")
+          .then((result) => {
+              return result.json();
+          }).then((res) => {
+              init(res);
+      });
+   }
